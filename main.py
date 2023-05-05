86441de7a94678c23101cbefb95d5f4a1cab5278
Wuu <1418605439@qq.com>
2023-05-05 23:55:47 +0800

    feat: 增加启动提醒

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import threading
 from concurrent.futures import ThreadPoolExecutor
 
 import win32api
+import win32con
 
 from utils.AudioUtil import get_all_audio_sessions, AudioUtil
 from utils.StrayUtil import StrayUtil
@@ -10,12 +11,11 @@ from utils.StrayUtil import StrayUtil
 TARGET_PROCESS_NAME_LIST = [
     "StarRail.exe"
 ]
-DEBUG = True
 if __name__ == '__main__':
     event = threading.Event()
     futures = []
     for session in get_all_audio_sessions():
-        if DEBUG or session.Process.name() in TARGET_PROCESS_NAME_LIST:
+        if session.Process.name() in TARGET_PROCESS_NAME_LIST:
             futures.append(
                 ThreadPoolExecutor().submit(AudioUtil(session).main_loop, event)
             )
@@ -25,6 +25,6 @@ if __name__ == '__main__':
         concurrent.futures.wait(futures)
         stray_util.exit_app()
     else:
-        win32api.MessageBox(0, "请先启动对应进程", "进程未找到", win32con.MB_ICONWARNING)
+        win32api.MessageBox(0, "请先启动游戏本体", "进程未找到", win32con.MB_ICONWARNING)
 
 # pyinstaller -Fw --add-data "resource/mute.ico;resource" -i "resource/mute.ico" -n background_muter.v0.1.2.exe main.py
--- utils/StrayUtil.py
+++ utils/StrayUtil.py
@@ -5,19 +5,25 @@ import pystray
 from PIL import Image
 
 
+def on_icon_ready(icon):
+    icon.visible = True
+    icon.notify("游戏关闭后自动退出，或从任务栏退出", "后台静音启动成功")
+
+
 class StrayUtil:
     event = None
 
     def __init__(self):
-        self.icon = pystray.Icon("mute")
-        self.icon.menu = pystray.Menu(
+        name = "后台静音"
+        menu = pystray.Menu(
             pystray.MenuItem("退出", self.exit_app)
         )
-        self.icon.icon = Image.open(pkg_resources.resource_filename(__name__, "../resource/mute.ico"))
+        icon = Image.open(pkg_resources.resource_filename(__name__, "../resource/mute.ico"))
+        self.icon = pystray.Icon(name, icon, name, menu)
 
     def run_app(self, event: threading.Event):
         self.event = event
-        self.icon.run_detached()
+        self.icon.run_detached(on_icon_ready)
 
     def exit_app(self):
         self.icon.stop()
