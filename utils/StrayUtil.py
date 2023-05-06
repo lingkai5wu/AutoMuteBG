f6a1cc9a7a083935a6a0963530a5df5ff72df2c6
Wuu <1418605439@qq.com>
2023-05-06 13:06:02 +0800

    feat: 增加开源地址跳转

--- utils/StrayUtil.py
+++ utils/StrayUtil.py
@@ -1,4 +1,5 @@
 import threading
+import webbrowser
 
 import pkg_resources
 import pystray
@@ -10,12 +11,17 @@ def on_icon_ready(icon):
     icon.notify("游戏关闭后自动退出，或从任务栏退出", "后台静音启动成功")
 
 
+def open_site():
+    webbrowser.open('https://gitee.com/lingkai5wu/AutoMuteBG')
+
+
 class StrayUtil:
     event = None
 
     def __init__(self):
         name = "后台静音"
         menu = pystray.Menu(
+            pystray.MenuItem("开源地址", open_site),
             pystray.MenuItem("退出", self.exit_app)
         )
         icon = Image.open(pkg_resources.resource_filename(__name__, "../resource/mute.ico"))
